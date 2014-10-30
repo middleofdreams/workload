5727772419577ee070a9818535bf6da16eeb472a
kuba <middleofdreams@gmail.com>
2014-10-30 09:01:06 +0100

    cleanup

--- db.py
+++ db.py
@@ -1,28 +1,33 @@
-import sqlite3,datetime
+import sqlite3
+import datetime
+
+
 class DB(object):
-    def __init__(self,parent):
-        self.parent=parent
+    def __init__(self, parent):
+        self.parent = parent
         #self.start()
-        self.db=sqlite3.connect("workload.db")
-        self.c=self.db.cursor()
-        
-    def addTask(self,taskname,taskpriority=5,context=1):
-        now=datetime.datetime.now().isoformat()
-        t=(taskname,taskpriority,context,now)
-        self.c.execute("INSERT into tasks ('taskname', 'priority','context','created') values (?,?,?,?)",t)
+        self.db = sqlite3.connect("workload.db")
+        self.c = self.db.cursor()
+
+    def addTask(self, taskname, taskpriority=5, context=1):
+        now = datetime.datetime.now().isoformat()
+        t = (taskname, taskpriority, context, now)
+        self.c.execute("INSERT into tasks ('taskname', 'priority','context',\
+            'created') values (?,?,?,?)", t)
         self.db.commit()
         for i in self.c.execute("SELECT last_insert_rowid()"):
             return i[0]
-        
-        
-    def getTasks(self,context):
-        t=(context,)
-        tasks=[]
-        for i in self.c.execute("SELECT rowid,taskname,priority FROM tasks where context=?",t): tasks.append(i)
+
+    def getTasks(self, context):
+        t = (context,)
+        tasks = []
+        for i in self.c.execute("SELECT rowid,taskname,priority \
+            FROM tasks where context=?", t):
+            tasks.append(i)
         return tasks
-    
-    def deleteTask(self,taskid):
-        t=(taskid,)
-        self.c.execute("Delete from tasks where rowid=?",t)
+
+    def deleteTask(self, taskid):
+        t = (taskid, )
+        self.c.execute("Delete from tasks where rowid=?", t)
         self.db.commit()
         print(taskid)
--- main.py
+++ main.py
@@ -1,132 +1,109 @@
 # -*- coding: utf-8 -*-
 
-from PySide import QtGui,QtCore
+from PySide import QtGui, QtCore
 from ui.main_ui import Ui_MainWindow
 from db import DB
 
+
 class Workload(QtGui.QMainWindow):
+
     def __init__(self):
         '''main window init'''
         QtGui.QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.setWindowFlags(QtCore.Qt.FramelessWindowHint|QtCore.Qt.WindowStaysOnTopHint)
-        
-        desktop=QtGui.QApplication.desktop()
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint
+                            | QtCore.Qt.WindowStaysOnTopHint)
 
-        self.move(10,(desktop.height()/2)-(self.height()))
-        
+        desktop = QtGui.QApplication.desktop()
+
+        self.move(10, (desktop.height() / 2) - (self.height()))
+
         self.show()
-        
-        self.ui.taskList.keyReleaseEvent=self.getKeysOnList
+
+        self.ui.taskList.keyReleaseEvent = self.getKeysOnList
         k = QtGui.QShortcut(self)
         k.setKey("Return")
         k.activated.connect(self.addTask)
         k = QtGui.QShortcut(self)
         k.setKey("Enter")
         k.activated.connect(self.addTask)
-        self.ui.taskList.setColumnWidth(0,20)           #zmieniona szerokość pierwszej kolumny
-        
-        self.currentContext=1 #tymczasowo
-        
-        self.db=DB(self)
+        self.ui.taskList.setColumnWidth(0, 20)
+        self.currentContext = 1  # tymczasowo
+
+        self.db = DB(self)
         self.loadTasksList()
 
-        
-    # TASKS RELATED ACTIONS   
+    # TASKS RELATED ACTIONS
     def addTask(self):
-        t=self.ui.taskInput.text().strip()
+        t = self.ui.taskInput.text().strip()
         self.ui.taskInput.clear()
-        priority=0
+        priority = 0
         try:
-            if t[1]==":":
-                priority=int(t[0])
-                t=t[2:]
-            
-            elif t[-2]==":":
-                priority=int(t[-1])
-                t=t[:-3]
+            if t[1] == ":":
+                priority = int(t[0])
+                t = t[2:]
+
+            elif t[-2] == ":":
+                priority = int(t[-1])
+                t = t[:-3]
         except:
             pass
-        
-        taskid=self.db.addTask(t)
-        self.createTaskItem(t, taskid,priority)
 
-        
-    def createTaskItem(self,t,taskid=None,priority=0):
+        taskid = self.db.addTask(t)
+        self.createTaskItem(t, taskid, priority)
+
+    def createTaskItem(self, t, taskid=None, priority=0):
         print(priority)
-        item=QtGui.QTreeWidgetItem(["",t])
-        item.setData(0,32,taskid)
-        item.setSizeHint(0, QtCore.QSize(0,22))         #zmieniona wysokość wiersza
+        item = QtGui.QTreeWidgetItem(["", t])
+        item.setData(0, 32, taskid)
+        item.setSizeHint(0, QtCore.QSize(0, 22))
         self.ui.taskList.addTopLevelItem(item)
-        
-        backColor = QtGui.QColor("#ff0000")                 #kolor tła kolumny (priorytet dla elementu)
-        item.setBackground(0, backColor)                #jak teraz juz tylko jakas funkcje na kolorowanie priorytetow i bedzie git : )
-        
-                                                        #z takich wizualnych rzeczy mozna dorobic jakas cienka ramke dookola okna.. bo na bialym tle sie to gubi;p
-        
-                                                
-                                                
-                                                
-                                                #teraz na takie cos wpadlem: jesli wkleisz do inputu tekst wiekszy niz>x to 
-                                                #otworzy sie to okienko podgladu zadania, bedzie mozna podejrzec cala tresc
-                                                #zadania i ustalic poprawny naglowek/tytul
-                                                
-                                                
-    def loadTasksList(self,archived=False):
+
+        backColor = QtGui.QColor("#ff0000")  # kolor tła kolumny
+        item.setBackground(0, backColor)     # (priorytet dla elementu)
+
+    def loadTasksList(self, archived=False):
         for i in self.db.getTasks(self.currentContext):
             self.createTaskItem(i[1], i[0])
-                                                
-                                                
-                                       
-  
-            
-    def deleteSelectedTask(self,force=False):
-        selectedItems=self.ui.taskList.selectedItems()
-        if len(selectedItems)>0:
-            item=self.ui.taskList.selectedItems()[0]
-            if force:self.deleteTask(item)
-            elif self.questionPopup("Delete task","Do you really want to delete task "+item.text(1)+"?"):
+
+    def deleteSelectedTask(self, force=False):
+        selectedItems = self.ui.taskList.selectedItems()
+        if len(selectedItems) > 0:
+            item = self.ui.taskList.selectedItems()[0]
+            if force:
                 self.deleteTask(item)
-               
-    
-    def deleteTask(self,item):
-            self.db.deleteTask(item.data(0,32))
-            index=self.ui.taskList.indexOfTopLevelItem(item)
+            elif self.questionPopup("Delete task",
+                "Do you really want to delete task " + item.text(1) + "?"):
+                self.deleteTask(item)
+
+    def deleteTask(self, item):
+            self.db.deleteTask(item.data(0, 32))
+            index = self.ui.taskList.indexOfTopLevelItem(item)
             self.ui.taskList.takeTopLevelItem(index)
-            
-            
-     
+
     # SHORTCUTS AND KEYBOARD EVENTS RELATED ACTIONS
-            
-    def getKeysOnList(self,e):
-        print(e.key())
-        if e.key()==16777223: #delete
-            force=False
-            print(e.modifiers)
+    def getKeysOnList(self, e):
+        print((e.key()))
+        if e.key() == 16777223:  # delete
+            force = False
             if (QtCore.Qt.ShiftModifier & e.modifiers()):
-                force=True
-            self.deleteSelectedTask(force)    
-            
-            
-            
-            
-            
-            
-    #ADDITIONAL FUNTIONS     
-            
-            
-    def questionPopup(self,title,msg):
-        resp=QtGui.QMessageBox.question(self,title,msg,buttons=QtGui.QMessageBox.Ok|QtGui.QMessageBox.Cancel)
-        if resp==QtGui.QMessageBox.Ok:
+                force = True
+            self.deleteSelectedTask(force)
+
+    #ADDITIONAL FUNTIONS
+    def questionPopup(self, title, msg):
+        resp = QtGui.QMessageBox.question(self, title, msg,
+        buttons=QtGui.QMessageBox.Ok | QtGui.QMessageBox.Cancel)
+        if resp == QtGui.QMessageBox.Ok:
             return True
         else:
             return False
-                                                
+
 if __name__ == "__main__":
     import sys
     app = QtGui.QApplication(sys.argv)
     myapp = Workload()
 
-    res=app.exec_()
+    res = app.exec_()
     sys.exit()
